9151923528c6d73c7d31e00650ad565580c1a8a9
Juan Cruz Nores <juancn@gmail.com>
2014-06-04 17:23:15 -0300

    Cleaner

--- jni/Darwin-defines.mk
+++ jni/Darwin-defines.mk
@@ -1,4 +1,4 @@
-MACHINE_LDFLAGS=-rpath @loader_path/../clang-all/build/Release+Asserts/lib
+MACHINE_LDFLAGS=-dynamiclib -rpath @loader_path/../clang-all/build/Release+Asserts/lib
 MACHINE_CFLAGS=-stdlib=libc++ 
 MACHINE_INCLUDE=-I$(JAVA_HOME)/include/darwin
 MACHINE_EXECUTABLE=$(OBJDIR)/libUnsafeDriver.dylib
