a47fbc6170d10b7e2ddb15c29474a6cd6d2ecd0f
Juan Cruz Nores <juancn@gmail.com>
2014-06-05 16:40:41 -0300

    Remove unused comment

--- src/test/resources/unsafe/example/avx.cpp
+++ src/test/resources/unsafe/example/avx.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Name        : avx2.cpp
-// Author      :
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C, Ansi-style
-//============================================================================
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <immintrin.h>
