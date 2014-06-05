063865815c488456245a5a4837b7d302ea7da74f
Juan Cruz Nores <juancruz@medallia.com>
2014-06-05 13:54:48 -0300

    build on linux with g++

--- jni/Linux-defines.mk
+++ jni/Linux-defines.mk
@@ -1,4 +1,4 @@
-MACHINE_LDFLAGS=-L/lib/x86_64-linux-gnu/
-MACHINE_CFLAGS=
+MACHINE_LDFLAGS=-shared -L/lib/x86_64-linux-gnu/ -pthread -Wl,--no-as-needed
+MACHINE_CFLAGS=-fPIC
 MACHINE_INCLUDE=
 MACHINE_EXECUTABLE=$(OBJDIR)/libUnsafeDriver.so
--- src/test/java/unsafe/example/HelloWorld.java
+++ src/test/java/unsafe/example/HelloWorld.java
@@ -11,8 +11,24 @@ public class HelloWorld {
 				"#include<stdio.h>\n" +
 						"extern \"C\" void foo() {" +
 						"printf(\"hello world!\\n\");" +
-						"}"
+						"}",
+		new String [] {
+"-I/usr/include/c++/4.8",
+"-I/usr/include/x86_64-linux-gnu/c++/4.8",
+"-I/usr/include/c++/4.8/backward",
+"-I/usr/lib/gcc/x86_64-linux-gnu/4.8/include",
+"-I/usr/local/include",
+"-I/usr/lib/gcc/x86_64-linux-gnu/4.8/include-fixed",
+"-I/usr/include/x86_64-linux-gnu",
+"-I/usr/include",
+		}
 		);
+		// Check for errors compiling
+		if (nativeModule.hasErrors()) {
+			System.out.println("Errors:\n " + nativeModule.getErrors());
+			return;
+		}
+
 		final NativeFunction foo = nativeModule.getFunctionByName("foo");
 		foo.invoke();
 	}
