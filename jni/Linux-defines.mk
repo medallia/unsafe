8826d0d32ea71dc758cd1de1f9bda04e3adcfde3
Juan Cruz Nores <juancruz@medallia.com>
2014-06-17 16:26:37 -0300

    Add rpath to linux. Still wc relative

--- jni/Linux-defines.mk
+++ jni/Linux-defines.mk
@@ -1,4 +1,4 @@
-MACHINE_LDFLAGS=-shared -L/lib/x86_64-linux-gnu/ -pthread -Wl,--no-as-needed
+MACHINE_LDFLAGS=-shared -L/lib/x86_64-linux-gnu/ -pthread -Wl,--no-as-needed -Wl,-rpath='$$ORIGIN/../clang-all/build/Release+Asserts/lib'
 MACHINE_CFLAGS=-fPIC
 MACHINE_INCLUDE=
 MACHINE_EXECUTABLE=$(OBJDIR)/libUnsafeDriver.so
