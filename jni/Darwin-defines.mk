70592da77054e8ddfb252a3c8e4982629967b745
eaf <eaf@asuka.medallia.com>
2015-05-28 23:25:25 -0300

    Fixing Linux build of the native library

--- jni/Darwin-defines.mk
+++ jni/Darwin-defines.mk
@@ -1,4 +1,5 @@
 MACHINE_LDFLAGS=-dynamiclib -rpath @loader_path/../clang-all/build/Release+Asserts/lib
 MACHINE_CFLAGS=-stdlib=libc++ 
 MACHINE_INCLUDE=-I$(JAVA_HOME)/include/darwin
+MACHINE_LIBS=-lz -lcurses
 MACHINE_EXECUTABLE=$(OBJDIR)/libUnsafeDriver.dylib
--- jni/Linux-defines.mk
+++ jni/Linux-defines.mk
@@ -1,4 +1,5 @@
 MACHINE_LDFLAGS=-shared -L/lib/x86_64-linux-gnu/ -pthread -Wl,--no-as-needed -Wl,-rpath='$$ORIGIN/../clang-all/build/Release+Asserts/lib'
 MACHINE_CFLAGS=-fPIC
-MACHINE_INCLUDE=
+MACHINE_INCLUDE=-I$(JAVA_HOME)/include/linux/
+ACHINE_LIBS=
 MACHINE_EXECUTABLE=$(OBJDIR)/libUnsafeDriver.so
